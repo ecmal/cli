5e598d7ab17d722831267379e679081cc06849ff
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-02-02 00:05:04 +0400

    first commit

---
+++ command.js
@@ -0,0 +1,15 @@
+System.register([], function(exports_1) {
+    var Command;
+    return {
+        setters:[],
+        execute: function() {
+            Command = (function () {
+                function Command() {
+                }
+                return Command;
+            })();
+            exports_1("Command", Command);
+        }
+    }
+});
+//# sourceMappingURL=command.js.map
---
+++ commander.js
@@ -0,0 +1,15 @@
+System.register([], function(exports_1) {
+    var Commander;
+    return {
+        setters:[],
+        execute: function() {
+            Commander = (function () {
+                function Commander() {
+                }
+                return Commander;
+            })();
+            exports_1("Commander", Commander);
+        }
+    }
+});
+//# sourceMappingURL=commander.js.map
